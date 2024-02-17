044ba5d5756c67b5801724ab5ab145534f1bfe8d
Cameron Spickert <cameron@checkhq.com>
2024-02-17 18:12:58 -0500

    Refine

--- todo/queries.py
+++ todo/queries.py
@@ -7,10 +7,16 @@ from todo import models
 security = HTTPBearer()
 
 
-def fetch_user(auth: str = Security(security)) -> models.User:
+def fetch_user(
+    auth: str = Security(security),
+) -> models.User:
+    """Fetches the user associated with the provided API key.
+
+    Raises an HTTP exception if the API key is invalid."""
+
     try:
         return models.User.objects.get(keys__key=auth.credentials)
-    except models.Key.DoesNotExist:
+    except models.User.DoesNotExist:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid or missing API key.",
@@ -21,6 +27,11 @@ def fetch_list(
     list_id: int,
     user: models.User = Depends(fetch_user),
 ) -> models.TodoList:
+    """Fetches the todo list with the provided ID.
+
+    Raises an HTTP 404 exception if the list does not exist for the current
+    user."""
+
     try:
         return user.lists.get(id=list_id)
     except models.TodoList.DoesNotExist:
@@ -34,6 +45,11 @@ def fetch_task(
     task_id: int,
     user: models.User = Depends(fetch_user),
 ) -> models.TodoTask:
+    """Fetches the todo task with the provided ID.
+
+    Raises an HTTP 404 exception if the task does not exist for the current
+    user."""
+
     try:
         return models.TodoTask.objects.filter(todo_list__user=user).get(id=task_id)
     except models.TodoTask.DoesNotExist:
--- todo/utils.py
+++ todo/utils.py
@@ -2,4 +2,6 @@ import uuid
 
 
 def generate_key() -> str:
+    """Returns a randomly generated 32-character string."""
+
     return uuid.uuid4().hex
