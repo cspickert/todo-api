01f032697620f7445128db7cb51e00f6983eb23d
Cameron Spickert <cameron@checkhq.com>
2024-03-11 16:35:47 -0400

    Add ReadMe webhook

--- todo/routers/__init__.py
+++ todo/routers/__init__.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from .lists import router as lists_router
 from .tasks import router as tasks_router
+from .webhooks import router as webhooks_router
 
 
 def register_routers(app: FastAPI):
     app.include_router(lists_router)
     app.include_router(tasks_router)
+    app.include_router(webhooks_router)
 
 
 __all__ = ["register_routers"]
---
+++ todo/routers/webhooks.py
@@ -0,0 +1,43 @@
+import os
+from typing import Annotated
+
+from fastapi import Depends, Header, Request, status
+from fastapi.exceptions import HTTPException
+from fastapi.routing import APIRouter
+from readme_metrics.VerifyWebhook import VerifyWebhook
+
+from todo import models
+from todo.queries import fetch_user
+
+router = APIRouter(prefix="/webhooks", tags=["webhooks"])
+
+# Webhooks
+
+
+README_SECRET = os.getenv("README_SECRET")
+
+
+@router.post(
+    "/readme",
+    status_code=status.HTTP_200_OK,
+    description="Handle ReadMe webhooks",
+)
+async def readme_webhook(
+    request: Request,
+    readme_signature: Annotated[str | None, Header()] = None,
+    # user: models.User = Depends(fetch_user),
+) -> dict:
+    # Verify the request is legitimate and came from ReadMe.
+    try:
+        VerifyWebhook(await request.json(), readme_signature, README_SECRET)
+    except Exception as error:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Invalid ReadMe webhook: {error}",
+        )
+    # Fetch the user from the database and return their data for use with OpenAPI variables.
+    # user = User.objects.get(email__exact=request.values.get("email"))
+    return {
+        # OAS Security variables
+        "HTTPBearer": "HTTPBearer",
+    }
